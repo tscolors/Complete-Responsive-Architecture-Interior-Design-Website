e636e7008ee278a82b82be9f1e3dc693152443b4
TIAGO <tscolors@hotmail.com>
2023-12-14 07:50:06 -0300

    1º Projeto finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function()
+{
+
+  $('.fa-bars').click(function()
+  {
+     $(this).toggleClass('fa-times')
+     $('.navbar').toggleClass('nav-toggle')
+  })
+
+
+  $(window).on('scroll load', function()
+  {
+      $('.fa-bars').removeClass('fa-times')
+      $('.navbar').removeClass('nav-toggle')
+  })
+
+  $('.count').each(function()
+  {
+     var $this = $(this),
+     countTo = $this.attr('data-count')
+     $({ countNum : $this.text()}).animate(
+      {
+        countNum : countTo
+      },
+      {
+        duration : 5000, step :function()
+        {
+          $this.text(Math.floor(this.countNum))
+        },
+        complete : function()
+        {
+          $this.text(this.countNum + '+')
+        }
+       }
+      )
+      
+  })
+
+
+  $('.project').magnificPopup({
+    delegate: 'a',
+    type:'image',
+    gallery:{
+      enabled:true
+    }
+  })
+
+
+
+
+})
